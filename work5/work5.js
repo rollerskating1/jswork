bafb30c0757aa0a6182b8344e7850f3169748c64
rollerskating1 <837202942@qq.com>
2019-10-14 11:51:48 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -9,3 +9,25 @@ for(let i=9;i>0;--i){
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
+str = '<table border="1">'
+for (let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+    str += '<td>' + j + '*' + i + '= ' +(j*i) + '</td>';
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
+
+str = '<table border="1">'
+for (let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=9;j>0;--j){
+    str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
