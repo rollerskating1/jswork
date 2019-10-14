9e7e977c09f99ae23ee464c756d66c70714cdb25
apple <837202942@qq.com>
2019-10-14 11:27:29 +0800

    work5

---
+++ work5/work5-1.js
@@ -0,0 +1,10 @@
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table2').innerHTML = str
---
+++ work5/work5-2.js
@@ -0,0 +1,22 @@
+
+void main()
+{
+ int ,i,j,a;
+ j=2;
+       for(a=1;a<=9;a++)
+    {
+     cout<<a<<"       ";
+    }
+   cout<<endl;
+ while (j<=9)
+  {
+      i=1;
+   while(i<=9)
+   {
+                cout<<i*j<<"       ";
+    i++;
+   }
+   cout<<endl;
+   j++;
+  }
+}
