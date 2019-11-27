5e5a81c8b54d0399eadb4abf6e2320ccd2c98e84
rollerskating1 <837202942@qq.com>
2019-11-27 13:40:26 +0800

    work13

--- work13/index.js
+++ work13/index.js
@@ -6,7 +6,7 @@ var elements = [
     option:{m:'男', w: '女'}},
     {tag: 'input', text: '爱  好', attr: {type: 'checkbox',name: 'hobby[]'},
 option:{swimming:'游泳', reading:'读书',running:'跑步'}},
-{tag: 'input', text: '住址',attr:{name:'area'},
+{tag: 'select', text: '住址',attr:{name:'area'},
 option: {'':'--请选择--',bj: '北京', sh:'上海',sz:'深圳'}},
 {tag:'textarea',text: '自我介绍:',
 attr: {name:'introduce',cols:'50',rows:'5'}},
