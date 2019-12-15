977ca28969a7cd7384fb18e1af3b7e50c71d7b65
rollerskating1 <837202942@qq.com>
2019-12-15 14:52:27 +0800

    work16

--- work16/index1.js
+++ work16/index1.js
@@ -1,32 +1,31 @@
 function animate(obj,option) {
-clearInterval(obj.timer);
-obj.timer = selInterval(function() {
-    var flag = true;
-    for (var k in option) {
-        var leader = parsetInt(getStyle(obj,k)) || 0;
-        var target = option[k];
-        var step = (target - leader) / 10;
-        step= step> 0 ? Math.cell(step): Math,floor(step);
-        leader = leader + step;
-        obj.style[k] = leader + 'px';
-        if(leader != target) {
-            flag = false;
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        var flag = true;
+        for (var k in option) {
+            var leader = parseInt(getStyle(obj,k)) || 0;
+            var target = option[k];
+            var step = (target - leader) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            leader = leader + step;
+            obj.style[k] = leader + 'px'
+            if (leader != target) {
+                flag = false;
+            }
         }
-    }
-    if(flag) {
-        clearInterval(obj,timer);
-    }
-},15);
+        if (flag) {
+            clearInterval(obj.timer);
+        }
+    },15);
 }
-function getStyle(obj,attr){
-    if (window.getComputedStyle){
-        return window.getComputedStyle(obj, null)[attr];
-}else {
-    return obj.currentStyle[attr];
-}
+function getStyle(obj,attr) {
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(obj,null)[attr];
+    }else{
+        return obj.currentStyle[attr];
+    }
 }
 var obj = document.getElementById('box');
 obj.onclick = function() {
-    animate(obj, {'left':200,'top':50});
-}
-
+    animate(obj, {'left':200,'top':300});
+};
