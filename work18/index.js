5bf9545c89873de4c8571800d060cc956444f304
rollerskating1 <837202942@qq.com>
2019-12-28 23:39:11 +0800

    work19

--- work18/index.js
+++ work18/index.js
@@ -2,21 +2,21 @@ function send(){
     let sno = document.getElementById('sno').value
     let name = document.getElementById('name').value
     let content = document.getElementById('content').value
-let xmlhttp;
-if (window.XMLHttpRequest){
-    xmlhttp = new XMLHttpRequest();
-}else{
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.open('post','http://139.9.81.203:8089/ajax',true)
-xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlhttp.send('sno=' + sno + '&name=' + name+"&content="+content)
-
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-        let ajaxData=JSON.parse(xmlhttp.responseText)
-        result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
-        document.getElementById("result").innerHTML = result.join('<br>');
+    let xmlhttp;
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-}
+    //xmlhttp.open('post','http://localhost:8080/ajax', true)
+    xmlhttp.open('post','http://139.9.81.203:8090/ajax', true)
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send('sno='+sno+'&name='+name+"&content="+content)
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            let ajaxData=JSON.parse(xmlhttp.responseText)
+            result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
+            document.getElementById("result").innerHTML = result.join('<br>');
+        }
+    }
 }
