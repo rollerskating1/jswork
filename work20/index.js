65cc891784c1e97019f49640edd1c97fc872de63
rollerskating1 <837202942@qq.com>
2019-12-30 12:00:16 +0800

    work20

---
+++ work20/index.js
@@ -0,0 +1,11 @@
+$('#fold>ul>li:first').find('.wrap').css({
+    display:'block'
+});
+$('#fold>ul>li').click(function(){
+    $(this).siblings('li').find('.wrap').css({
+        display:'none'
+    });
+    $(this).find('.wrap').css({
+        display:'block'
+    });
+});
---
+++ work20/index1.js
@@ -0,0 +1,12 @@
+$('#toRight').click(function(){
+    $('#right>select').append($('#left>select>option:selected'));
+});
+$('#toLeft').click(function(){
+    $('#left>select').append($('#right>select>option:selected'));
+});
+$('#toAllRight').click(function(){
+    $('#right>select').append($('#left>select>option'));
+});
+$('#toAllLeft').click(function(){
+    $('#left>select').append($('#right>select>option'));
+});
