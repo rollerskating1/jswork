dba617bbcb25222a999c768346a5ddcaff35d5c2
rollerskating1 <837202942@qq.com>
2020-01-03 00:06:35 +0800

    work21

--- work21/index.js
+++ work21/index.js
@@ -1,62 +1,66 @@
-$(function () {
-    var i =0;
+$(function(){
+    var i = 0;
     var timer = null;
     var delay = 1000;
     var width = 1420;
     var speed = 400;
     var firstimg = $('.hot li').first().clone();
-    $('hot').append(firstimg).width($('.hot li').length * width);
-timer = setInterval(imgChange,delay);
-$('.banner').hover(function () {
-    clearInterval(timer);
-},function () {
-    timer = setInterval(imgChange,delay);  
-});
-$('dot li').mouseover(function () {
-    i = $(this).index();
-    $('.hot').stop().animate({
-        left: -i * width
-    },200);
-    dotChange();
-  });
-  $('.banner').hover(function () {
-      imgChange()
+    $('.hot').append(firstimg).width($('.hot li').length * width);
+    timer = setInterval(imgChange,delay);
+    $('.banner').hover(function(){
+        clearInterval(timer);
+    },function(){
+        timer = setInterval(imgChange,delay);
     });
-    $('.prev').click(function () {
---i;
-if(i == -1){
-    i = $('.hot li').length - 2;
-    $('.hot').css({
-        left:-($('.hot li').length - 1) * width
-    })
-}
-$('.hot').stop().animate({
-    left:-i * width
-},speed);
-dotChange();
-});
-function imgChange() {
-    ++i;
-    isCrack();
-    dotChange();
-  }
-  function isCrack() {
-      if(i == $('.hot li').length){
-          i = 1;
-          $('hot').css({
-              left:0
-          });
-      }
-      $('.hot').stop().animate({
-          left:-i * width
-      },speed);
+    $('.dot li').mouseover(function(){
+        i = $(this).index();
+        $('.hot').stop().animate({
+            left: -i * width
+        },200);
+        dotChange();
+    });
+    $('.banner').hover(function(){
+        $('.arrow').show();
+    },function(){
+        $('.arrow').hide();
+    });
+    $('.next').click(function(){
+        imgChange()
+    });
+    $('.prev').click(function(){
+        --i;
+        if(i == -1){
+            i = $('.hot li').length - 2;
+            $('.hot').css({
+                left: -($('.hot li').length - 1) * width
+            });
+        }
+        $('.hot').stop().animate({
+            left: -i * width
+        },speed);
+        dotChange();
+    });
+    function imgChange(){
+        ++i;
+        isCrack();
+        dotChange();
     }
-    function dotChange() {
-        if (i == $('.hot li').length - 1){
+    function isCrack(){
+        if(i == $('.hot li').length){
+            i = 1;
+            $('.hot').css({
+                left: 0
+            });
+        }
+        $('.hot').stop().animate({
+            left: -i * width
+        },speed);
+    }
+    function dotChange(){
+        if(i == $('.hot li').length - 1){
             $('.dot li').eq(0).addClass('on').siblings().removeClass('on');
         }else{
             $('.dot li').eq(i).addClass('on').siblings().removeClass('on');
         }
-        }
+    }
 });
-
--- work21/index1.js
+++ work21/index1.js
@@ -1,5 +1,5 @@
 $("input:last").click(function () {
-    $.post("/ajax",$('fotm').serialize(),function (res) {
+    $.post("/ajax",$('form').serialize(),function (res) {
         result= res.reverse().map((val)=>{return JSON.stringify(val)+"<br>"})
         $('#result').empty().append($("<div></div>").html(result))
       })
